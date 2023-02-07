ecb6e799e8e114423b900d9479b5d04d5ffa67df
Roshan Guragain <twistfreaky@gmail.com>
2023-02-07 21:26:43 +0545

    backend setup

---
+++ sharepic_backend/schemas/comments.js
@@ -0,0 +1,17 @@
+export default{
+    name:'comments',
+    title:'Comment',
+    type:'document',
+    fields:[
+        {
+            name:'postedBy',
+            title:'PostedBy',
+            type:'postedBy'
+        },
+        {
+            name:'comment',
+            title:'comment',
+            type:'string'
+        },
+    ]
+}
---
+++ sharepic_backend/schemas/index.js
@@ -0,0 +1,7 @@
+import comments from "./comments";
+import pin from "./pin";
+import postedBy from "./postedBy";
+import save from "./save";
+import user from "./user";
+
+export const schemaTypes = [user,pin,comments,postedBy,save]
